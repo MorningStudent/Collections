8efbc740bc89b3ada49614e1cd3675b98380fe27
VictorCorlateanu <126675107+VictorCorlateanu@users.noreply.github.com>
2023-07-15 16:45:35 +0300

    Add files via upload
    This version has added the Faker library and has implemented generating of some properties for the <Product> and <Client> classes.

--- caseThird/App.java
+++ caseThird/App.java
@@ -1,19 +1,23 @@
 package caseThird;
+import com.github.javafaker.Faker;
+
 
 public class App {
     public static void main(String[] args) throws ItemStockQuantityLackException {
 
+        Faker faker = new Faker();
+
         Stock stock = new Stock();
 
-        Product product1 = new Product(1001, "Samsung XXII", new Money(1000, Money.Currency.EUR));
-        Product product2 = new Product(1002, "Samsung XXIII", new Money(1100, Money.Currency.EUR));
-        Product product3 = new Product(1003, "Samsung XXIV", new Money(1200, Money.Currency.EUR));
+        Product product1 = new Product(faker.number().numberBetween(1, 2000), faker.commerce().productName(), new Money(1000, Money.Currency.EUR));
+        Product product2 = new Product(faker.number().numberBetween(1, 2000), faker.commerce().productName(), new Money(1100, Money.Currency.EUR));
+        Product product3 = new Product(faker.number().numberBetween(1, 2000), faker.commerce().productName(), new Money(1200, Money.Currency.EUR));
 
         stock.addItem(new Item<>(product1, 10));
         stock.addItem(new Item<>(product2, 10));
         stock.addItem(new Item<>(product3, 10));
         
-        Cart cart = new Cart(new Client("Cristina Valcan", "+37369777222"), stock);
+        Cart cart = new Cart(new Client(faker.name().fullName(), faker.phoneNumber().cellPhone()), stock);
 
 
         cart.addItem(new Item<>(product1, 3));
